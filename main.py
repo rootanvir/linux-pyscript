082b800bc7a32c33904ea362f4b30c668780c7e2
Tanvir <tanvir@kali>
2026-07-19 03:15:38 +0600

    nmap+

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import subprocess as sb
+import re
 web = input("Enter a website name : ")
-
+print("Looking for address...")
 output = sb.run(
-    ["ping","-c","4",web],
+    ["host",web],
+    capture_output=True,
+    text=True,
+)
+result = re.search(r'has address (\d+\.\d+\.\d+\.\d+)',output.stdout)
+print(web," ip address : ",result.group(1))
+ipadd = result.group(1)
+print("Scaning for open port...")
+output = sb.run(
+    ["nmap","-T4",ipadd],
     capture_output=True,
     text=True,
 )
