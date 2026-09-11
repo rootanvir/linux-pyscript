ab178720025bf808f83f3a9ffd9af86912a1202c
Tanvir Ahmed Tuhin <103141061+rootanvir@users.noreply.github.com>
2026-09-11 19:07:27 +0600

    Improve website IP lookup and port scanning
    Refactor IP address retrieval and enhance port scanning.

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import subprocess as sb
 import re
+
 web = input("Enter a website name : ")
 print("Looking for address...")
 output = sb.run(
-    ["host",web],
+    ["host", web],
     capture_output=True,
     text=True,
-)
-result = re.search(r'has address (\d+\.\d+\.\d+\.\d+)',output.stdout)
-print(web," ip address : ",result.group(1))
-ipadd = result.group(1)
-print("Scaning for open port...")
-output = sb.run(
-    ["nmap","-T4",ipadd],
-    capture_output=True,
-    text=True,
-)
-print(output.stdout)
+    )
+result = re.search(r'has address (\d+\.\d+\.\d+\.\d+)', output.stdout)
+if result:
+    ipadd = result.group(1)
+    print(web, "ip address :", ipadd)
+    print("Scanning for open ports and server details...")
+    output = sb.run(
+        ["nmap", "-T5", "-sV", ipadd],
+        capture_output=True,
+        text=True,
+    )
+    print(output.stdout)
+else:
+    print("Could not find an IPv4 address.")
